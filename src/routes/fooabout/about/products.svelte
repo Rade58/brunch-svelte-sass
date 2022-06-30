197112685f4f2d0b078e30fd132fd0187cec32b6
Rade58 <bajic.rade2@gmail.com>
2022-07-01 01:47:18 +0200

    🦉

--- src/routes/fooabout/about/products.svelte
+++ src/routes/fooabout/about/products.svelte
@@ -1,1 +1,24 @@
-<h1>Products</h1>
+<script lang="ts">
+
+  let count = 0;
+
+  function handleClick(){
+    count++;
+  } 
+
+</script>
+
+<div>{count}</div>
+<button
+  class="btn btn-secondary"
+  on:click={handleClick}
+>
+  Increment
+</button>
+<section>
+  <!-- JUST TO BE CLEAR
+    YOU CAN'T USE TERNARY FOR CONDITIONAL RENDERING OF COMPONENTS
+  BUT YOU CAN DO CONDITIONAL RENDERING OF VALUES WITH TERNARY -->
+  {count > 5 ? "FOO" : "BAR"}
+</section>
+
