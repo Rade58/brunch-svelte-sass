83dd753bfb91f85cd4f3df41738130e2abaa2f81
Rade58 <bajic.rade2@gmail.com>
2022-07-01 02:11:02 +0200

    🦉

--- src/routes/fooabout/contact.svelte
+++ src/routes/fooabout/contact.svelte
@@ -1,9 +1,31 @@
 <script lang="ts">
-  import Food from '$components/food.svelte'
+
+  let count = 0;
+
+  // AS count CHANGES, THIS
+  // IS NOT GOING TO BE CHANGED
+  // ONLY WILL HAVE VALUE WITH WHOOM IT IS INITIALIZED
+  // AND THAT IS      0  TIMES   2
+  let doubleFirst = count * 2;
+
+  // BUT THIS ONE THAT I LABLED WITH $  WILL BE CHANGED
+  // AS count CHANGES
+
+  $: doubleSecond = count * 2; 
 
 </script>
 
-<h1>Contact</h1>
-<h1>
-  <Food color="crimso" count={6} />
-</h1>
+
+<button
+  class="btn btn-primary"
+  on:click={() => {count++}}
+>
+  Increment
+</button>
+
+<div>
+  doubleFirst: <strong>{doubleFirst}</strong>
+</div>
+<div>
+  doubleSecond: <strong>{doubleSecond}</strong>
+</div>
