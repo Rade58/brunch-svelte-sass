5642e57b586c5f0ff8680854e81ed8d7af95c213
Rade58 <bajic.rade2@gmail.com>
2022-07-01 22:35:50 +0200

    🦉

--- src/components/Money.svelte
+++ src/components/Money.svelte
@@ -1,3 +1,13 @@
 <script>
-  export let shibaCoin = 2000
+  export let shibaCoin = 2000;
+  export let safeMoon = 4600;
 </script>
+
+<section>
+  {shibaCoin}
+</section>
+<section>
+  {safeMoon}
+</section>
+
+
