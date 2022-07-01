79df4cafb9d4842c1a1906c8c0746a7f7a5ffec0
Rade58 <bajic.rade2@gmail.com>
2022-07-01 23:08:08 +0200

    🦉

--- src/components/Money.svelte
+++ src/components/Money.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
-  export let shibaCoin = 2000;
-  export let safeMoon = 4600;
+
+  // SEE HERE WE HAVE A VARIABLE
+  let youCanRock: boolean = true;
+
+  // YOU HAVE A FUNCTION THAT IS DOING REASSIGNMENT
+  function toggleRocking() {
+
+    // THIS IS GOING TO DO RESIGNMENT OF THIS VARIABLE
+    // AND DEPENDING OF THAT WE WILL SEE DIFFERENT VALUE RENDERING
+    // WHEN WE PASS THIS VALUE TO HTML
+    youCanRock = !youCanRock
+  }
+
+  // THIS IS ONLY GOING TO BE AFFECTED BY INITIAL VALUE OF  youCanRock
+  let youCanSwim: boolean;
+  youCanSwim = youCanRock;
+
+  // REACTIVE DECLARATION IS GOING TO BE AFFECTED BY 
+  // EVERY VALUE CHANGE OF THE   youCanRock      VARIABLE
+  $: youCanDab = youCanRock
+
 </script>
 
 <section>
-  {shibaCoin}
+  Rock: {youCanRock}
 </section>
 <section>
-  {safeMoon}
+ Swim: {youCanSwim}
 </section>
+<section>
+ Dab {youCanDab}
+</section>
+<button
+  on:click={toggleRocking}
+  class="btn btn-primary"
+>
+  toggle
+</button>
 
 
