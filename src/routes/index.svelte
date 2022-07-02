0c83986c7be16b0b8f67788ecaf2fb31701e08b2
Rade58 <bajic.rade2@gmail.com>
2022-07-02 19:40:11 +0200

    🦉

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,7 @@
   import Some from '$components/Some.svelte'
   import Deferred from '$components/Deferred.svelte'
   import Move from '$components/Move.svelte'
+  import Once from '$components/Once.svelte'
 </script>
 
 
@@ -9,8 +10,9 @@
 
 <h1>Welcome to SvelteKit</h1>
 
-<Some />
-<Deferred />
-<Move />
+  <Some />
+  <Deferred />
+  <Move />
+  <Once />
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
