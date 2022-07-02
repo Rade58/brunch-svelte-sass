e32eb33f2dec2ce09aeb420f1d40183c901c80a5
Rade58 <bajic.rade2@gmail.com>
2022-07-02 17:50:01 +0200

    🦉

--- src/components/Deferred.svelte
+++ src/components/Deferred.svelte
@@ -7,7 +7,7 @@
         if(num>50){
           res("Hello World")
         }else{
-          rej()
+          rej("Hello Error")
         }
       }, 1000)
     })
@@ -24,7 +24,7 @@
   <div class="alert alert-error shadow-lg">
     <div>
       <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-      <span>Error! Task failed successfully.</span>
+      <span>{error}! Task failed successfully.</span>
     </div>
   </div>
   {/await}
