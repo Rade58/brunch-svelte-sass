8d921da86f7551c3a6b31f3c3c69293322b9c636
Rade58 <bajic.rade2@gmail.com>
2022-07-02 21:03:55 +0200

    🦉

---
+++ src/components/RandomChild.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  // LETS SAY THAT THIS COMPONENT ALSO LISTNS FOR SHIBA EVENT
+
+  const handleShibaPayload = (e: any) => {
+    console.log(e.payload.data)
+    console.log("FROM RANDOM CHILD")
+  }
+
+</script>
