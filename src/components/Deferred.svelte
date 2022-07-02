e0908dea2f22dcb13eaeb8db221e2b74096f7b49
Rade58 <bajic.rade2@gmail.com>
2022-07-02 17:41:22 +0200

    🦉

---
+++ src/components/Deferred.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+
+  const makeFakeRequest = () => {
+    return new Promise((res, rej) => {
+      setTimeout(() => {
+        const num = Math.round(Math.random() * 100)
+        if(num>50){
+          res("Hello World")
+        }else{
+          rej()
+        }
+      }, 1000)
+    })
+  }
+
+</script>
+
+<section>
+  {#await makeFakeRequest()}
+    <progress class="progress w-56"></progress>
+  {:then val}
+    <div>{val}</div>
+  {:catch error}
+  <div class="alert alert-error shadow-lg">
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current flex-shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+      <span>Error! Task failed successfully.</span>
+    </div>
+  </div>
+  {/await}
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,6 @@
 <script>
   import Some from '$components/Some.svelte'
+  import Deferred from '$components/Deferred.svelte'
 </script>
 
 
@@ -8,5 +9,6 @@
 <h1>Welcome to SvelteKit</h1>
 
 <Some />
+<Deferred />
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
