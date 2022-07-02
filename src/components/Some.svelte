d092f5f225959864006f320ea01fcb2951aeba7a
Rade58 <bajic.rade2@gmail.com>
2022-07-02 02:42:55 +0200

    🦉

---
+++ src/components/Some.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  
+  const colors = [
+    {id: 2, color: "#ffef96"},
+    {id:4, color: "#50394c"},
+    {id: 3, color: "#b2b2b2"},
+    {id: 1, color: "#f4e1d2"},
+  ]
+
+</script>
+
+<h1>Palette</h1>
+<div>
+  {#each colors as color (color.id)}
+    <div style="background-color: {color.color}" class={`grid w-32 h-20 rounded text-primary-content place-content-center`}>{color.color}</div> 
+  {/each}
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
+<script>
+  import Some from '$components/Some.svelte'
+</script>
+
+
+
+
 <h1>Welcome to SvelteKit</h1>
+
+<Some />
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
