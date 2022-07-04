c927893f268be3159b42ade2b077d57a2e76cfba
Rade58 <bajic.rade2@gmail.com>
2022-07-04 17:52:54 +0200

    🦉

--- src/components/Foo.svelte
+++ src/components/Foo.svelte
@@ -1,9 +1,38 @@
 <script lang="ts">
-  let inputData = ""
-  $: console.log({inputData})
+  let textInput = ""
+
+  // THIS WILL HOLD VALUE OF NUMERIC INPUT
+  let numericValue: number;
+
+  // THIS WILL HOLD VALUE OF RANGE INPUT
+  let rangeValue: number;
+
+  $: console.log(typeof numericValue, typeof rangeValue)
 
 </script>
 
 <input 
-  bind:value={inputData}
+  bind:value={textInput}
 />
+
+<!-- NUMERIC INPUT -->
+<input
+  type="number"
+  placeholder="enter number"
+  class="input input-bordered w-full max-w-xs"
+  min={0}
+  max={6}
+  bind:value={numericValue}
+/>
+
+<br />
+
+
+<!-- RANGE INPUT -->
+<input type="range"
+  min="0" max="100"
+  bind:value={rangeValue} 
+  class="range range-primary" 
+/>
+
+
