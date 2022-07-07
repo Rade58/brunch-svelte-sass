010ac993f6dd0fe715fbd191e8ca5ce63e64a6ef
Rade58 <bajic.rade2@gmail.com>
2022-07-07 20:48:20 +0200

    🦉

---
+++ src/components/SomeThing.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+
+</script>
+
+
+<!-- WE ARE GOING TO ADD FEW CLASSES WITH A BUTTON
+JUST PAY ATTENTION HOW WE ARE ADDING CLASSES -->
+
+
+<img 
+  class="mask mask-heart"
+  src="https://placeimg.com/160/160/arch" 
+/>
