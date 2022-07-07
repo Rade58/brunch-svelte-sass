db7fd2b24ceae8b7f1efbfad2df4e3182b676e3d
Rade58 <bajic.rade2@gmail.com>
2022-07-08 00:00:31 +0200

    🦉

--- src/components/FooThingThree.svelte
+++ src/components/FooThingThree.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+
+  // HERE YOU GO
+  // YOU WILL GET BOOLEANS
+  console.log($$slots) //   {child-one: true, child-two: true}
+
+  // YOU CAN RENDER THINGS BASED ON IF CERTAIN IS SLOT PASSED OR NOT
+
+</script>
+
+
 
 <section>
   <h1>Hello World</h1>
